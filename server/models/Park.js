533e0a46698077a4c031ec59dc094b467b46f96b
Jenna Leopold <jleopold424@gmail.com>
2022-03-24 09:28:14 -0400

    added Park schema

---
+++ server/models/Park.js
@@ -0,0 +1,12 @@
+const { Schema } = require('mongoose')
+
+const Park = new Schema(
+  {
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    rides: [{ type: Schema.Types.ObjectId, ref: 'Ride' }]
+  },
+  { timestamps: true }
+)
+
+module.exports = Park
