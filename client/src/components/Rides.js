309f6351a7b440f6ee2c4f71cd9f74d498244022
pregan23 <pregan2bing@gmail.com>
2022-03-24 16:30:07 -0400

    RideCard

--- client/src/components/Rides.js
+++ client/src/components/Rides.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import RideCard from './components/RideCard'
 
-const Rides = ( { rides } ) => {
-    return rides.map((ride) => <RideCard key={ride._id} {...ride} />)
+const Rides = ({ rides }) => {
+  return (
+    <div>
+      <h3>Rides</h3>
+      <div className="rides-wrapper">
+        {rides.map((ride) => (
+          <RideCard key={ride._id} {...ride} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Rides
