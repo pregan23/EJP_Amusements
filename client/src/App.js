83f44984650200823fb460a09a220c99d37bdfe5
pregan23 <pregan2bing@gmail.com>
2022-03-24 14:21:57 -0400

    rideform

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import CreateRideForm from './components/CreateRideForm'
 import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
+
+
+
+
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [minHeight, setMinHeight] = useState('')
+  const [imagePath, setImagePath] = useState('')
+
+  const handleNameChange = (event) => {
+    setName(event.targer.value)
+  }
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.targer.value)
+  }
+
+  const handleMinHeightChange = (event) => {
+    setMinHeight(event.targer.value)
+  }
+
+  const handleImagePathChange = (event) => {
+    setImagePath(event.targer.value)
+  }
+
+  const createRide = async (event) => {
+    event.preventDefault()
+    const response = await axios.post('http://localhost:3001/rides', {
+      name: name,
+      description: description,
+      minHeight: minHeight,
+      imagePath: imagePath
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    // setSearchResults(response.data.results)
+    setName('')
+    setDescription('')
+    setMinHeight('')
+    setImagePath('')
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateRideForm />
     </div>
   );
 }
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
 
+router.post('/rides', controllers.createRide)
+
 module.exports = router
