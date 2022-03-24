92783f0148166192a9cd806a79873bcb6f41392a
Jenna Leopold <jleopold424@gmail.com>
2022-03-24 16:21:19 -0400

    add .rides-wrapper around RideCards

--- client/src/components/Rides.js
+++ client/src/components/Rides.js
@@ -2,7 +2,16 @@ import React from 'react'
 import RideCard from './components/RideCard'
 
 const Rides = ({ rides }) => {
-  return rides.map((ride) => <RideCard key={ride._id} {...ride} />)
+  return (
+    <div>
+      <h3>Rides</h3>
+      <div className="rides-wrapper">
+        {rides.map((ride) => (
+          <RideCard key={ride._id} {...ride} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Rides
