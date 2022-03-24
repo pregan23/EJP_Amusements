3ae20113024501dacfe21416bc60a0265f46fc93
Jenna Leopold <jleopold424@gmail.com>
2022-03-24 15:45:31 -0400

    define getAllRides controller

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,16 @@ const createRide = async (req, res) => {
   }
 }
 
+const getAllRides = async (req, res) => {
+  try {
+    const rides = await Ride.find()
+    return res.status(200).json({ rides })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  createRide
+  createRide,
+  getAllRides
 }
