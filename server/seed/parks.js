09d2dc9f74ddffd22a9adf87c7db051ef5fc81aa
pregan23 <pregan2bing@gmail.com>
2022-03-24 10:31:31 -0400

    parks

--- server/seed/parks.js
+++ server/seed/parks.js
@@ -5,7 +5,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
 
-    const parks = {
+    const park = {
         name: 'EJP Amusements', 
         description: 'Party poopers need not apply!  The greatest collection of thrill rides on this coast, EJP Amusements will blow you away!', 
         location: 'Antarctica',
@@ -13,8 +13,8 @@ const main = async () => {
         
     }
 
-    await Plant.insertMany(plants)
-    console.log("Created some plants!")
+    await Park.insertOne(park)
+    console.log("Created an amusement park!")
 }
 const run = async () => {
     await main()
