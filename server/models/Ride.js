f0c18e8cdc57d1f2a5c560e9e7a91f9d4d0f323e
Eric Lantz <ericmlantz@gmail.com>
2022-03-24 09:55:09 -0400

    Removed parkId from Ride Schema

--- server/models/Ride.js
+++ server/models/Ride.js
@@ -7,7 +7,6 @@ const Ride = new Schema(       //Matches the file name and is singular and Pasca
     image: {type: String, required: true},
     minHeight: { type: String, required: true},
     //isOpen: { type: Boolean, required: true},
-    parkId: { type: String, required: true},
   },
   { timestamps: true }        //The timestamps option tells mongoose to assign createdAt and updatedAt fields to your schema. The type assigned is Date. By default, the names of the fields are createdAt and updatedAt. Customize the field names by setting timestamps.createdAt and timestamps.updatedAt
 )
